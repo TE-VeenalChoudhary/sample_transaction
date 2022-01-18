1d7e811c7df71d29c476ceec7fd325d152019236
Veenal Choudhary <vc86974@gmail.com>
2022-01-18 17:44:40 +0530

    added pojo

---
+++ nachpay-sample/src/main/java/com/te/nachpaysample/pojo/Relationship.java
@@ -0,0 +1,61 @@
+package com.te.nachpaysample.pojo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="np_relationship")
+public class Relationship {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="relationship_id")
+	private Long relationshipId;
+	
+	@Column(name="seller_id")
+	private Long sellerId;
+	
+	@Column(name="buyer_id")
+	private Long buyerId;
+	
+	@JsonIgnore
+	@ManyToMany
+	@JoinColumn(name="seller_id",referencedColumnName = "company_id" ,insertable=false ,updatable=false)
+	@JoinColumn(name="buyer_id",referencedColumnName = "company_id" ,insertable=false ,updatable=false)
+	private Company company;
+
+	public Long getRelationshipId() {
+		return relationshipId;
+	}
+
+	public void setRelationshipId(Long relationshipId) {
+		this.relationshipId = relationshipId;
+	}
+
+	public Long getSellerId() {
+		return sellerId;
+	}
+
+	public void setSellerId(Long sellerId) {
+		this.sellerId = sellerId;
+	}
+
+	public Long getBuyerId() {
+		return buyerId;
+	}
+
+	public void setBuyerId(Long buyerId) {
+		this.buyerId = buyerId;
+	}
+	
+	
+
+}
