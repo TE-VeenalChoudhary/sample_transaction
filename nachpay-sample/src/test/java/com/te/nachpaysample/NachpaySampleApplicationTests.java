fe5db821e3217addbd31bac02a5bc6df7a8eb6fd
Veenal Choudhary <vc86974@gmail.com>
2022-01-18 16:59:40 +0530

    first commit sample

---
+++ nachpay-sample/src/test/java/com/te/nachpaysample/NachpaySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.te.nachpaysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NachpaySampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
